7ec9feb3ebe6a243aca05a2092c7756e1ec68aa0
Anastasiia Kosiak <anastasiia.kosiak@vitechteam.com>
2022-05-09 13:37:34 +0300

    added code snippets from lesson two

---
+++ src/main/java/lesson/two/ScannerExample.java
@@ -0,0 +1,20 @@
+package lesson.two;
+
+import java.util.Scanner;
+
+public class ScannerExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        double firstNum = sc.nextDouble();
+        System.out.println(firstNum);
+        System.out.println("Enter the second number");
+        double secondNum = sc.nextDouble();
+        System.out.println(secondNum);
+        // 1. +: 45 + 56 + print
+        // 2. -: 45 - 56  + print
+        //3. *: 45 * 56 + print
+        //4. /: 45 / 56 + print
+
+    }
+}
---
+++ src/main/java/lesson/two/TypesExample.java
@@ -0,0 +1,21 @@
+package lesson.two;
+
+public class TypesExample {
+    public static void main(String[] args) {
+        boolean bool = true;
+        byte b = 67; // 8 бітів => -2^7 - 2^7 +1 = -256 - 255
+        System.out.println(b);
+        short sh = 567;
+        System.out.println(sh);
+        int i = 56778;
+        System.out.println(i);
+        int iNegative = -56;
+        System.out.println(iNegative);
+        long l = 560000000;
+        float f = 56.78f; //floating point - плаваюча крапка - із десяткою частиною
+        System.out.println(f);
+        double d = 567.4555;
+        System.out.println(d);
+
+    }
+}
